c362ae176b4bde88eaf8004157cb50dcf983f2f6
dipesh <amodedipesh12@gmail.com>
2026-04-05 12:43:38 +0530

    feat:implemented data layer with remote sources and repo implementation

---
+++ lib/features/auth/data/model/auth_user_model.dart
@@ -0,0 +1,33 @@
+import '../../domain/entities/auth_user.dart';
+
+class AuthUserModel {
+  final String id;
+  final String name;
+  final String email;
+  final String? avatarUrl;
+  final String accessToken;
+  final String refreshToken;
+
+  const AuthUserModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    this.avatarUrl,
+    required this.accessToken,
+    required this.refreshToken,
+  });
+
+  factory AuthUserModel.fromJson(Map<String, dynamic> json) {
+    return AuthUserModel(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      email: json['email'] as String,
+      avatarUrl: json['avatar_url'] as String?,
+      accessToken: json['access_token'] as String,
+      refreshToken: json['refresh_token'] as String,
+    );
+  }
+
+  AuthUser toEntity() =>
+      AuthUser(id: id, name: name, email: email, avatarUrl: avatarUrl);
+}
