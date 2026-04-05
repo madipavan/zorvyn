5f0f74e0f4fa9faea17c5bccd55adcc0d5564f71
dipesh <amodedipesh12@gmail.com>
2026-04-05 21:35:44 +0530

    feat:updated local host url

--- lib/core/network/api_enpoints.dart
+++ lib/core/network/api_enpoints.dart
@@ -1,6 +1,6 @@
 class ApiEnpoints {
   static final String productionBaseUrl = "/api";
-  static final String localBaseUrl = "http://192.168.1.4:7203/api";
+  static final String localBaseUrl = "http://0.0.0.0:10000/api";
   static final String login = "/auth/login";
   static final String register = "/auth/register";
   static final String logout = "/auth/logout";
